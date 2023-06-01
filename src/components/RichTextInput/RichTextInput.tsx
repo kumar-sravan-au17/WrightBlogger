e2b85c09bf0ed35badd6b9a4476291c8f4bc8f5a
Sravan Kumar <sravan.kumar@launchventures.co>
2023-06-01 13:16:31 +0530

    remove unused import

--- src/components/RichTextInput/RichTextInput.tsx
+++ src/components/RichTextInput/RichTextInput.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import { useRef } from 'react'
 import "./RichTextInput.css"
 import JoditEditor from 'jodit-react'
 import { RichTextInputProps } from '../../Types'
